99a167b07ddee82b64deaac0e14d7ce7d6e6c6a7
Dorian Peters <dpeters08@gmail.com>
2025-08-13 00:36:29 -0700

    fix: remove unnecessary build configuration from vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/',
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: `[name].js`,
-        assetFileNames: '[name].[ext]',
-        chunkFileNames: '[name].min.js',
-      },
-    },
-  }
+  base: '/'
 });
